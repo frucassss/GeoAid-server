573e84d86642ecdecd6176bbc19c503207a7d73e
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-11-09 10:48:38 +0100

    added users database with dummy data

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -1,5 +1,18 @@
+drop table if exists quotes;
+
 create table quotes
 (
     id    int auto_increment,
     quote varchar(255)
+);
+
+drop table if exists users;
+
+create table users
+(
+    id       int auto_increment,
+    firstName varchar(255),
+    lastName varchar(255),
+    location varchar(255),
+    premium  varchar(255)
 );
--- src/main/resources/db-populate.sql
+++ src/main/resources/db-populate.sql
@@ -1,2 +1,7 @@
 INSERT INTO quotes VALUES (0, 'You must accept that you might fail.');
 INSERT INTO quotes VALUES (1, 'Everyone enjoys doing the kind of work for which he is best suited.');
+insert into users values (0, 'John', 'Doe', 'New York', 'none');
+insert into users values (1, 'Jane', 'Doe', 'New York', 'company');
+insert into users values (2, 'John', 'Smith', 'New York', 'standaard');
+insert into users values (3, 'Jane', 'Smith', 'New York', 'none');
+insert into users values (4, 'John', 'Doe', 'London', 'none');
