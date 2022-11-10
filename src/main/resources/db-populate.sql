2f1140ce987572a5c94503dfe181240ac7f64c69
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-11-10 10:13:08 +0100

    added dummy data to table

--- src/main/resources/db-populate.sql
+++ src/main/resources/db-populate.sql
@@ -5,3 +5,8 @@ INSERT INTO users VALUES (0, 'John', 'Doe', '123 Main Street', 'none');
 INSERT INTO users VALUES (1, 'Jane', 'Doe', '123 Main Street', 'company');
 INSERT INTO users VALUES (2, 'John', 'Smith', '123 Main Street', 'company');
 INSERT INTO users VALUES (3, 'Jane', 'Smith', '123 Main Street', 'standard');
+
+INSERT INTO domes VALUES (0, 'Dome 1', '40.0', '40.0');
+INSERT INTO domes VALUES (1, 'Dome 2', '40.1', '40.0');
+INSERT INTO domes VALUES (2, 'Dome 3', '40.2', '40.0');
+INSERT INTO domes VALUES (3, 'Dome 4', '40.3', '40.0');
