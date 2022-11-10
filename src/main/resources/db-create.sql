88751644e222b4b72ed55e17322975828d024deb
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-11-10 10:12:47 +0100

    added domes database

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -16,3 +16,13 @@ create table users
     homeAddress varchar(255),
     premium  varchar(255)
 );
+
+drop table if exists domes;
+
+create table domes
+(
+    id       int auto_increment,
+    domeName varchar(255),
+    latitude varchar(255),
+    longitude varchar(255)
+);
