4648f1123dcd23ddec78c1c8d0550296b85ea48c
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-11-17 09:55:40 +0100

    fix of error

--- src/test/java/be/howest/ti/mars/logic/controller/MockMarsController.java
+++ src/test/java/be/howest/ti/mars/logic/controller/MockMarsController.java
@@ -1,8 +1,11 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.domain.Dome;
 import be.howest.ti.mars.logic.domain.Quote;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+
+import java.util.List;
 
 public class MockMarsController implements MarsController {
     private static final String SOME_QUOTE = "quote";
@@ -23,6 +26,11 @@ public class MockMarsController implements MarsController {
 
     @Override
     public void deleteQuote(int quoteId) {
+    }
+
+    @Override
+    public List<Dome> getDomes() {
+        return null;
     }
 
 }
